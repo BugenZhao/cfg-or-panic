45b34ed42b4f99468586ad4cc455906b48d6f90a
Bugen Zhao <i@bugenzhao.com>
2023-08-29 19:30:32 +0800

    support expanding mod
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- cfg-or-panic/src/lib.rs
+++ cfg-or-panic/src/lib.rs
@@ -1,23 +1,54 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::ToTokens;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, parse_quote, Attribute, Block, Item, ItemFn, ItemImpl};
 
 #[proc_macro_attribute]
 pub fn cfg_or_panic(args: TokenStream, input: TokenStream) -> TokenStream {
     let args: proc_macro2::TokenStream = args.into();
-    let mut function = parse_macro_input!(input as ItemFn);
 
-    let block = function.block;
-    function.block = {
-        let block = quote::quote! {{
+    let mut item = parse_macro_input!(input as Item);
+
+    match &mut item {
+        Item::Fn(item_fn) => expand_fn(&args, item_fn),
+        Item::Impl(item_impl) => expand_impl(&args, item_impl),
+        Item::Mod(_item_mod) => todo!(),
+        _ => todo!(),
+    }
+
+    item.into_token_stream().into()
+}
+
+fn expand_impl(args: &TokenStream2, item_impl: &mut ItemImpl) {
+    for item in &mut item_impl.items {
+        match item {
+            syn::ImplItem::Fn(impl_item_fn) => expand_impl_fn(&args, impl_item_fn),
+            _ => {}
+        }
+    }
+}
+
+fn expand_fn(args: &TokenStream2, item_fn: &mut ItemFn) {
+    expand_fn_inner(args, &mut item_fn.block, &mut item_fn.attrs);
+}
+
+fn expand_impl_fn(args: &TokenStream2, item_fn: &mut syn::ImplItemFn) {
+    expand_fn_inner(args, &mut item_fn.block, &mut item_fn.attrs);
+}
+
+fn expand_fn_inner(args: &TokenStream2, fn_block: &mut Block, fn_attrs: &mut Vec<Attribute>) {
+    let block = fn_block.clone();
+    *fn_block = parse_quote! {
+        {
             #[cfg(not(#args))]
             unimplemented!("function unimplemented under `#[cfg(not({}))]`", stringify!(#args));
             #[cfg(#args)]
             #block
-        }}
-        .into();
-        parse_macro_input!(block as syn::Block).into()
+        }
     };
 
-    function.into_token_stream().into()
+    let attr = parse_quote! {
+        #[cfg_attr(not(#args), allow(unused_variables))]
+    };
+    fn_attrs.push(attr);
 }
--- cfg-or-panic/tests/basic.rs
+++ cfg-or-panic/tests/basic.rs
@@ -1,6 +1,18 @@
 use cfg_or_panic::cfg_or_panic;
 
 #[cfg_or_panic(not_set)]
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+struct Foo;
+
+#[cfg_or_panic(not_set)]
+impl Foo {
+    fn bar(&self) {}
+}
+
+#[cfg_or_panic(not_set)]
 #[test]
 fn test() {
     println!("Hello, world!");
